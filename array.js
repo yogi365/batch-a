57ecd32bf9ad151bd6378d464c94a285a98ae8bd
yogi365 <sharma.yogendra@yahoo.com>
2023-09-01 15:48:56 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,22 @@
+var a = [];
+var arr = new Array();
+
+// a = [1,5,6,"Abc","Abanana",undefined,true, null];
+// a[12]="Grapes";
+// console.log(a[2])
+/*
+    push, pop, shift, unshift, splice, sort, indexOf
+*/
+// a.sort();
+
+function abc(){
+    console.log(a);
+}
+
+a = [1,2,3,[54,12,13]];
+a[4]=abc();
+console.log(a[4]);
+
+console.log("24"+true)
+
+"2" 2 
